3de4c172539108f5194151a08893331c27ce497e
diyornv <diyorcode@gmail.com>
2025-07-27 00:13:23 +0500

    finaly

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import WeatherWidget from './components/WeatherWidget'
+import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <WeatherWidget />
+    </ThemeProvider>
   </StrictMode>,
 )
